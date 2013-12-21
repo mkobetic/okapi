e29214cf8f51dee8e7c2b1fc0020d4bb9def1f54
Martin Kobetic <mkobetic@gmail.com>
2013-12-20 23:49:23 -0500

    more tests

--- libcrypto/hash_test.go
+++ libcrypto/hash_test.go
@@ -20,9 +20,27 @@ func TestEvpHash(t *testing.T) {
 	if hex.EncodeToString(digest) != "098f6bcd4621d373cade4e832627b4f6" {
 		t.Fail()
 	}
+	count, err := md5.Write([]byte("test"))
+	if err == nil || count != 0 {
+		t.Fail()
+	}
 }
 
-func TestOkapiHash(t *testing.T) {
-	md5 := okapi.MD5.New()
-	defer md5.Close()
+func TestHashCloning(t *testing.T) {
+	sha := okapi.SHA256.New()
+	defer sha.Close()
+	sha.Write([]byte("test"))
+	sha2 := sha.Clone()
+	digest := sha2.Digest()
+	if hex.EncodeToString(digest) != "9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08" {
+		t.Fail()
+	}
+	count, err := sha.Write([]byte("test"))
+	if err != nil || count != 4 {
+		t.Fail()
+	}
+	digest = sha.Digest()
+	if hex.EncodeToString(digest) != "37268335dd6931045bdcdf92623ff819a64244b53d0e746d438797349d4da578" {
+		t.Fail()
+	}
 }
