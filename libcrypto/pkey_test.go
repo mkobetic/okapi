b205bcaf0255bf1b7d87a4a7decd822728a089e2
Martin Kobetic <mkobetic@gmail.com>
2014-01-03 13:13:29 -0500

    add DSA test

--- libcrypto/pkey_test.go
+++ libcrypto/pkey_test.go
@@ -17,3 +17,14 @@ func TestRSA(t *testing.T) {
 		t.Fatal("RSA decryption failed")
 	}
 }
+
+func TestDSA_SHA256(t *testing.T) {
+	pri := DSA_SHA256Size(2048)
+	pub := pri.PublicKey()
+	plain := []byte("Message in a bottle!")
+	encrypted := pub.Encrypt(plain)
+	decrypted := pri.Decrypt(encrypted)
+	if !bytes.Equal(plain, decrypted) {
+		t.Fatal("RSA decryption failed")
+	}
+}
